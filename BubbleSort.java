cb7a1097308a5e09b57b0ed7ebd6e8e7e608ebea
AKMatos <90463973+AKMatos@users.noreply.github.com>
2025-08-27 07:23:28 -0400

    Broken Java File

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+//A simple algorithm for bubble sorting an array.
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+            
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length-1-i;) {
+                
+                if (arr[j] > arr[j+1]) {
+
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[j]);
+        }
+    }
+}
