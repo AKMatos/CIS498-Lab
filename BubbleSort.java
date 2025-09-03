2131dfaff22efdb1d41baf29e8c4514d498562a5
Ryan Callaghan <rcallaghan@umassd.edu>
2025-09-03 11:30:01 -0400

    fixed sorting issue

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,23 +4,22 @@ public class BubbleSort {
 
     public static void main(String[] args) {
             
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
 
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = 0; j < arr.length-1-i; j++) {
                 
-                if (arr[j] < arr[j+1]) {
-
-                    temp = arr[j];
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
                     arr[j] = arr[j+1];
+		    arr[j+1] = temp;
                 }
             }
         }
 
         for (int i = 0; i < arr.length; i++) {
-
-            System.out.println(arr[j]);
+            System.out.println(arr[i]);
         }
     }
 }
