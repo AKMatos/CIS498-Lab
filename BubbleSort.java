057e9f857d1695b3546881ea11b0bfa65afa7cbc
ZMFarley <zacharymfarley@gmail.com>
2025-09-03 11:51:51 -0400

    Fixed Bubblesort

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,23 +4,23 @@ public class BubbleSort {
 
     public static void main(String[] args) {
             
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
-
+        int arr[] = {6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
+        int temp = 0;
         for (int i = 0; i < arr.length; i++) {
 
-            for (int j = 0; j < arr.length-1-i; j++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
                 
-                if (arr[j] < arr[j+1]) {
+                if (arr[j] > arr[j+1]) {
 
                     temp = arr[j];
                     arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
 
         for (int i = 0; i < arr.length; i++) {
-
-            System.out.println(arr[j]);
+            System.out.println(arr[i]);
         }
     }
 }
