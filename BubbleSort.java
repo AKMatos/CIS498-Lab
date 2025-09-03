94d158fb880e16cd285b93d84832ed66efbf02ba
JacobMcPhee <Mcjakemc@gmail.com>
2025-09-03 12:10:22 -0400

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,10 +1,12 @@
 //A simple algorithm for bubble sorting an array.
 
+//A simple algorithm for bubble sorting an array.
+
 public class BubbleSort {
 
     public static void main(String[] args) {
             
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
 
         for (int i = 0; i < arr.length; i++) {
 
@@ -12,15 +14,16 @@ public class BubbleSort {
                 
                 if (arr[j] < arr[j+1]) {
 
-                    temp = arr[j];
+                    int temp = arr[j];
                     arr[j] = arr[j+1];
+                    arr[j+1] =  temp;
                 }
             }
         }
 
         for (int i = 0; i < arr.length; i++) {
 
-            System.out.println(arr[j]);
+            System.out.println(arr[arr.length-1-i]);
         }
     }
-}
+}}
