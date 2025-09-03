80ac611a97517ef3eb2e05094ef90f72317c570d
awetos <78708899+awetos@users.noreply.github.com>
2025-09-03 12:51:48 -0400

    bubble sort working

--- BubbleSort.java
+++ BubbleSort.java
@@ -15,6 +15,7 @@ public class BubbleSort {
 
                     temp = arr[j];
                     arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
