b5fe14ca31c1eb39ab2177f3f1465befc8a3658a
Jared-Dedeus <jdedeus@umassd.edu>
2025-09-03 12:27:08 -0400

    Commit 1

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,26 +1,25 @@
-//A simple algorithm for bubble sorting an array.
-
+// A simple algorithm for bubble sorting an array.
+ 
 public class BubbleSort {
-
+ 
     public static void main(String[] args) {
-            
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
-
+       
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9 }; // added missing semicolon
+ 
         for (int i = 0; i < arr.length; i++) {
-
-            for (int j = 0; j < arr.length-1-i; j++) {
-                
-                if (arr[j] < arr[j+1]) {
-
-                    temp = arr[j];
-                    arr[j] = arr[j+1];
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+               
+                if (arr[j] > arr[j + 1]) {  // use > for ascending order
+                    int temp = arr[j];      // declare temp
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;      // complete swap
                 }
             }
         }
-
+ 
+        // Print sorted array
         for (int i = 0; i < arr.length; i++) {
-
-            System.out.println(arr[j]);
+            System.out.println(arr[i]); // fixed index
         }
     }
 }
