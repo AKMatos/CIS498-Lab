3fac2409703c3103c6ad6c3eea59333198baed5e
SeanIversDev <seaniversdev@gmail.com>
2025-09-03 12:51:53 -0400

    Add files via upload
    Fixed bubble sort algorithm

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,23 +4,25 @@ public class BubbleSort {
 
     public static void main(String[] args) {
             
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
 
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = 0; j < arr.length-1-i; j++) {
                 
-                if (arr[j] < arr[j+1]) {
+                if (arr[j] > arr[j+1]) {
 
-                    temp = arr[j];
-                    arr[j] = arr[j+1];
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+
                 }
             }
         }
 
         for (int i = 0; i < arr.length; i++) {
 
-            System.out.println(arr[j]);
+            System.out.println(arr[i]);
         }
     }
 }
