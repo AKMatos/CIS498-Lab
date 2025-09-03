f2ca5f10318c87090a35b6512b1777f38b9d9f02
Jess Welsh <147105191+jessautumn@users.noreply.github.com>
2025-09-03 12:23:54 -0400

    Add files via upload
    git commit -m "Updated BubbleSort code (web)"

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,25 +2,22 @@
 
 public class BubbleSort {
 
-    public static void main(String[] args) {
-            
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
-
+    public static void main(String[] args) {   
+        int temp ; // err 1 = declare temp
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9} ;  //err 2 = missing semi
         for (int i = 0; i < arr.length; i++) {
-
             for (int j = 0; j < arr.length-1-i; j++) {
-                
-                if (arr[j] < arr[j+1]) {
-
+                if (arr[j] > arr[j+1]) { // err 3 = sorted incrementing, instead of decrementing
                     temp = arr[j];
                     arr[j] = arr[j+1];
+                    arr[j+1] = temp ;   // err 4 = added to filter all values
                 }
             }
         }
 
         for (int i = 0; i < arr.length; i++) {
-
-            System.out.println(arr[j]);
-        }
+            System.out.println(arr[i]); //err 5 = passing through wrong var
+        }                 
     }
+    // The sixth error is that there is not six errors, there are five. :)))
 }
