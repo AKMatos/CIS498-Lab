3c933d8390fb9d654bae793125602d8ffcc3f470
ebedard1 <ebedard1008@gmail.com>
2025-09-03 12:00:43 -0400

    lab
     On branch ethan_bedard_terminal
     Changes to be committed:
    	modified:   BubbleSort.java
    
     Changes not staged for commit:
    	modified:   README.md

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,26 +1,28 @@
 //A simple algorithm for bubble sorting an array.
 
-public class BubbleSort {
+public class BubbleSort 
+{
+    public static void main(String[] args) 
+    {
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
+        int temp;
 
-    public static void main(String[] args) {
-            
-        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9}
-
-        for (int i = 0; i < arr.length; i++) {
-
-            for (int j = 0; j < arr.length-1-i; j++) {
-                
-                if (arr[j] < arr[j+1]) {
-
+        for (int i = 0; i < arr.length; i++) 
+        {
+            for (int j = 0; j < arr.length-1-i; j++) 
+            {
+                if (arr[j] > arr[j+1]) 
+                {
                     temp = arr[j];
                     arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-
-            System.out.println(arr[j]);
+        for (int i = 0; i < arr.length; i++) 
+        {
+            System.out.println(arr[i]);
         }
     }
 }
